4308e4a152969ec9cabed69637d84dfc28ec5387
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-07-23 22:49:30 +0530

    optimized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSounds from "./ToggleSounds";
 
@@ -9,28 +9,30 @@ function App() {
   // Will be be AM or PM
   const partOfDay = time.slice(-2);
 
-  const workouts = [
-    {
-      name: "Full-body workout",
-      numExercises: partOfDay === "AM" ? 9 : 8,
-    },
-    {
-      name: "Arms + Legs",
-      numExercises: 6,
-    },
-    {
-      name: "Arms only",
-      numExercises: 3,
-    },
-    {
-      name: "Legs only",
-      numExercises: 4,
-    },
-    {
-      name: "Core only",
-      numExercises: partOfDay === "AM" ? 5 : 4,
-    },
-  ];
+  const workouts = useMemo(() => {
+    return [
+      {
+        name: "Full-body workout",
+        numExercises: partOfDay === "AM" ? 9 : 8,
+      },
+      {
+        name: "Arms + Legs",
+        numExercises: 6,
+      },
+      {
+        name: "Arms only",
+        numExercises: 3,
+      },
+      {
+        name: "Legs only",
+        numExercises: 4,
+      },
+      {
+        name: "Core only",
+        numExercises: partOfDay === "AM" ? 5 : 4,
+      },
+    ];
+  }, [partOfDay]);
 
   function formatTime(date) {
     return new Intl.DateTimeFormat("en", {
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -9,14 +9,45 @@ function Calculator({ workouts, allowSound }) {
   const [duration, setDuration] = useState(0);
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+
+  // const playSound = useCallback(
+  //   function () {
+  //     if (!allowSound) return;
+  //     const sound = new Audio(clickSound);
+  //     sound.play();
+  //   },
+  //   [allowSound]
+  // );
+
+  useEffect(
+    function () {
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    },
+    [number, sets, speed, durationBreak]
+  );
+
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+      playSound();
+    },
+    [duration, allowSound]
+  );
+
+  function handleInc() {
+    setDuration((duration) => Math.floor(duration) + 1);
+  }
+
+  function handleDec() {
+    setDuration((duration) => (duration > 1 ? Math.floor(duration) - 1 : 0));
+  }
+
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 
   return (
     <>
@@ -67,13 +98,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && "0"}
           {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
